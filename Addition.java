eb8367474bc771ec0935d15cd985918cbc91963b
NagaAsrithM <asrith12787@gmail.com>
2025-01-16 23:21:27 +0530

    added

--- Addition.java
+++ Addition.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class Addition {
+public class Addition
+{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
 }
